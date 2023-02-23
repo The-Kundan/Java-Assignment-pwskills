03e83586773b136c3000a570554a07e601675a88
Kundan kumar <106131038+The-Kundan@users.noreply.github.com>
2023-02-23 23:40:12 +0530

    Java_variable&Data types Assignment pwskills

---
+++ Test4.java
@@ -0,0 +1,10 @@
+package Variable.DataTypes_Assignment;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int x=2;
+        int cube=x*x*x;
+        System.out.println("The side is : "+x);
+        System.out.println("The cube is : "+cube);
+    }
+}
---
+++ Test5.java
@@ -0,0 +1,16 @@
+package Variable.DataTypes_Assignment;
+
+public class Test5 {
+    public static void main(String[] args) {
+        int num1=2;
+        int num2=3;
+        int temp;
+        System.out.println("The first number before swap is : "+num1);
+        System.out.println("The second number before swap is : "+num2);
+        temp=num1;
+        num1=num2;
+        num2=temp;
+        System.out.println("The first number after swap is : "+num1);
+        System.out.println("The second number after swap is : "+num2);
+    }
+}
